a101b081233ecff5e16975a759c035464dcb0e12
Geoff-Walker <geoffwalker1980@gmail.com>
2021-12-04 12:21:15 +0000

    Html, css and object constructor complete

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let library = [];
+
 function Book(title, author, pages, read) {
 	this.title = title;
 	this.author = author;
@@ -5,9 +7,19 @@ function Book(title, author, pages, read) {
 	this.read = read;
 }
 Book.prototype.info = function() {
-	console.log(`${title} by ${author}, ${pages} pages, ${(read = true ? 'I have read this book' : 'not read yet')}`);
+	console.log(
+		`${this.title} by ${this.author}, ${this.pages} pages, ${(this.read = true
+			? 'I have read this book'
+			: 'not read yet')}`
+	);
 };
 
 const bible = new Book('The Bible', 'God', 1200, true);
+const fellowship = new Book('The fellowship of the Ring', 'JRR Tolkein', 423, false);
+const twoTowers = new Book('The Two Towers', 'JRR Tolkein', 352, false);
+const kingReturn = new Book('The Return of the King', 'JRR Tolkein', 345, false);
+const hobbit = new Book('The Hobbit', 'JRR Tolkein', 310, true);
 
-bible.info();
+library.push(bible, fellowship, twoTowers, kingReturn, hobbit);
+console.log(library);
+console.log(library[2].title);
